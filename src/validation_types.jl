90473a717ced1511631588cda07c5ad32e4326cf
Mal Miller <59854849+mmiller-max@users.noreply.github.com>
2021-12-04 11:49:40 +0000

    Improve displaying of ValidationError

--- src/validation_types.jl
+++ src/validation_types.jl
@@ -24,10 +24,14 @@ end
 function Base.show(io::IO, ::MIME"text/plain", err::ValidationError)
     printstyled(io, "GQLError", color=Base.error_color())
     printstyled(io, "\n      message: ", err.message, color=Base.error_color())
-    if !isnothing(err.locations)
-        printstyled(io, "\n  location(s): $(err.locations[1])", color=Base.error_color())
-        for i in 2:length(err.locations)
-            printstyled(io, "\n               $(err.locations[i])", color=Base.error_color())
+    if !isnothing(err.locations) && !isempty(err.locations)
+        if length(err.locations) == 1
+            printstyled(io, "\n     location: $(err.locations[1])", color=Base.error_color())
+        else
+            printstyled(io, "\n  location(s): $(err.locations[1])", color=Base.error_color())
+            for i in 2:length(err.locations)
+                printstyled(io, "\n               $(err.locations[i])", color=Base.error_color())
+            end
         end
     end
 end
